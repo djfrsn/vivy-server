a3a35f7f956bbdfdae82002c7917b8a87ccc4e42
djfrsn <dj@dennisjefferson.com>
2015-12-13 23:02:19 -0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,43 @@
+Vivy ~ JS Server
+==============================================================
+"There are no secrets, only things less talked about."
+
+Built with:
+ 
+ * [Node](http://nodejs.org/)
+ * [Express](http://expressjs.com/)
+ * [Webpack](https://webpack.github.io)
+ * [React](https://facebook.github.io/react/)
+ * [Redux](http://rackt.org/redux/index.html)
+
+## How to Use It
+
+Clone this repository 
+
+	git clone http://git.4over.com/dennisj/4over-server.git .
+
+#### Install local node modules
+
+  'npm install'
+
+## Download the app & start the server
+
+  'npm run-script vivy'
+
+## Dev Usage
+
+  'npm start'
+
+## Production Deployment
+
+  'npm run-script deploy'
+
+## Based on
+
+https://github.com/picanteverde/init
+
+http://www.christianalfoni.com/articles/2015_04_19_The-ultimate-webpack-setup
+
+http://www.jchapron.com/2015/08/14/getting-started-with-redux/
+
+https://github.com/erikras/react-redux-universal-hot-example
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, './public/app/shell.js')
+  ],
+  output: {
+    path: path.join(__dirname, './dist'),
+    chunkFilename: "app/[id].js",
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    // used to load css & js in seperate modules
+    // new ExtractTextPlugin('css/styles.extract.css', {
+    //   allChunks: true
+    // }),
+    // Avoid loading duplicate code
+    new webpack.optimize.DedupePlugin(),
+    // Optimize module load order in bundles
+    new webpack.optimize.OccurenceOrderPlugin(),
+    // new webpack.ProvidePlugin({
+    //     _: "underscore"
+    // }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    // create common modules shared between entry points
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "commons",
+    //   // (the common chunk name)
+ 
+    //   filename: "commons.js"
+    //   // (the filename of the commons chunk)
+
+    //   // minChunks: 3,
+    //   // (Modules must be shared between 3 entries)
+
+    //   // chunks: ["pageA", "pageB"],
+    //   // (Only use these entries)
+    // }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    // __dirname '../..' 
+    // Copy files to dist
+    new CopyWebpackPlugin([
+        // Files 
+        { from: './public/images/favicon.ico', to: './favicon.ico' },
+        { from: './public/config/robots.txt', to: './robots.txt' },
+    ]),
+  ],
+  postcss: [
+    require('lost')
+  ],
+  module: {
+    // preLoaders: [
+    //   { test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader' }
+    // ],
+    loaders: [
+      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style-loader!css-loader!postcss-loader" },
+      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url?limit=10000!img?progressive=true' },
+      { test:  /\.json?$/, loader: 'url?limit=10000!img?progressive=true' }
+    ]
+  },
+  resolve: {
+    root: [
+      './node_modules'
+    ],
+    extensions: ['', '.js'],
+    // use to point to folders for imports node style
+    modulesDirectories: [
+      './public'
+    ]
+    // set an alias for dependencies in node_modules or other dirs
+    // alias: {
+    // }
+  }
+};
