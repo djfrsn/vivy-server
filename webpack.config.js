2ea4d58bd14076f80224c08adf16dc8aa7fc64e7
djfrsn <dj@dennisjefferson.com>
2015-12-14 18:24:49 -0800

    Add post css

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
     // ],
     loaders: [
       { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
       { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url?limit=10000!img?progressive=true' },
       { test:  /\.json?$/, loader: 'url?limit=10000!img?progressive=true' }
     ]
